1bbff5f20e54255755c53a28e6c851f36673e83d
DB <deepb@treepiesinc.com>
2018-01-24 14:36:46 +0530

    Done First Commit

---
+++ TrialFramework/TrialFramework/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  TrialFramework
+//
+//  Created by DB on 24/01/18.
+//  Copyright © 2018 DB. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init(){}
+    
+    public static func DB() -> String {
+     
+        return "Let's Build are Custom Pod."
+        
+    }
+    
+}
---
+++ TrialFramework/TrialFramework/TrialFramework.h
@@ -0,0 +1,19 @@
+//
+//  TrialFramework.h
+//  TrialFramework
+//
+//  Created by DB on 24/01/18.
+//  Copyright © 2018 DB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrialFramework.
+FOUNDATION_EXPORT double TrialFrameworkVersionNumber;
+
+//! Project version string for TrialFramework.
+FOUNDATION_EXPORT const unsigned char TrialFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrialFramework/PublicHeader.h>
+
+
